4109b0e6d4b3a24ddcde03c8286fa31a0a280c5f
SotaShirakawa <souta060614@gmail.com>
2023-04-30 02:12:19 +0900

    完成

--- Count Arrange/ViewController.swift
+++ Count Arrange/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var countLabel: UILabel!
+    @IBOutlet var busted: UILabel!
+    var randomNumber = 0
+    var currentCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func startButtonPressed(_ sender: Any) {
+        // ランダムな数字を生成
+        randomNumber = Int.random(in: 1...10)
+        countLabel.text = "Count: 0"
+        currentCount = 0
+        // BUSTEDリセット
+        busted.text = "ドキドキ..."
+    
+    }
+    
+    @IBAction func countButtonPressed(_ sender: Any) {
+        currentCount += 1
+        countLabel.text = "Count: \(currentCount)"
+        
+        if currentCount == randomNumber {
+            busted.text = "BUSTED"
+        }
+    }
 }
-
